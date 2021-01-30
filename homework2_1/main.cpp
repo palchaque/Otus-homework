f0d97ab021614ea5240f6da10b9f9c667b6f1c96
Palchaque <48860195+palchaque@users.noreply.github.com>
2021-01-30 15:48:53 +0300

    Update main.cpp

--- homework2_1/main.cpp
+++ homework2_1/main.cpp
@@ -107,3 +107,39 @@ int getFactorial(const int &number)
 }
 
 //#define RUN_BENCHMARK
+
+#ifndef RUN_BENCHMARK
+
+int main()
+{
+
+    auto myMap = std::map<int, int, std::less<int>, custom_allocator<std::pair<int, int>, 10>>{};
+
+    for (int i = 0; i<= 9; i++)
+    {
+      myMap.emplace(i, getFactorial(i));
+    }
+
+    for (auto mapIterator : myMap)
+    {
+        std::cout<<mapIterator.first<<" "<<mapIterator.second<<std::endl;
+    }
+
+
+    custom_list<int, custom_allocator<int, 10>, 10> list;
+
+    for (int i = 0; i<= 9; i++)
+    {
+        list.push_back(i);
+    }
+
+    for (int i = 0; i<= 9; i++)
+    {
+        std::cout<<list.get(i)<<std::endl;
+    }
+
+    return 0;
+
+}
+#endif
+
