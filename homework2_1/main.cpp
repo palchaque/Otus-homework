f5c5841cf5cd8d3c1a3a574012f825fcb295d714
Palchaque <48860195+palchaque@users.noreply.github.com>
2021-02-05 18:31:41 +0300

    Update main.cpp

--- homework2_1/main.cpp
+++ homework2_1/main.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <map>
 #include <memory>
-#include <benchmark/benchmark.h>
 
 template<typename T, size_t max_size>
 class custom_allocator
@@ -141,79 +140,4 @@ int main()
 #endif
 
 
-#ifdef RUN_BENCHMARK
 
-static void insert_into_map(benchmark::State& state) {
-    for (auto _ : state)
-    {
-        auto myMap = std::map<int, int>{};
-
-        for (int i = 0; i<= 9; i++)
-        {
-          myMap.emplace(i, getFactorial(i));
-        }
-
-        for (auto mapIterator : myMap)
-        {
-            //std::cout<<mapIterator.first<<" "<<mapIterator.second<<std::endl;
-        }
-    }
-
-}
-
-static void insert_into_map_with_allocator(benchmark::State& state) {
-    for (auto _ : state)
-    {
-
-        auto myMap = std::map<int, int, std::less<int>, custom_allocator<std::pair<int, int>, 10>>{};
-
-        for (int i = 0; i<= 9; i++)
-        {
-          myMap.emplace(i, getFactorial(i));
-        }
-
-        for (auto mapIterator : myMap)
-        {
-            //std::cout<<mapIterator.first<<" "<<mapIterator.second<<std::endl;
-        }
-    }
-
-}
-
-static void insert_into_custom_container_with_default_allocator(benchmark::State& state) {
-    for (auto _ : state)
-    {
-        custom_list<int, std::allocator<int>, 10> list;
-
-        for (int i = 0; i<= 9; i++)
-        {
-            list.push_back(i);
-        }
-    }
-
-}
-
-static void insert_into_custom_container_with_custom_allocator(benchmark::State& state) {
-    for (auto _ : state)
-    {
-        custom_list<int, custom_allocator<int, 10>, 10> list;
-
-        for (int i = 0; i<= 9; i++)
-        {
-            list.push_back(i);
-        }
-    }
-
-}
-
-BENCHMARK(insert_into_map);
-
-BENCHMARK(insert_into_map_with_allocator);
-
-BENCHMARK(insert_into_custom_container_with_default_allocator);
-
-BENCHMARK(insert_into_custom_container_with_custom_allocator);
-
-BENCHMARK_MAIN();
-
-#endif
