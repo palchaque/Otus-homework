edee1305eb582b8dab68094e17cd3717c49363a0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-19 09:17:40 +0000

    Deploy palchaque/Otus-homework to github.com/palchaque/Otus-homework.git:gh-pages

---
+++ homework6_1/CMakeFiles/bulk.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bulk.dir/main.cpp.o"
+  "bulk"
+  "bulk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bulk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
